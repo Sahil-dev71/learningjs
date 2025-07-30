ccaff388c63b19e73edf106b3371fe8eafbba78c
Sahil-dev71 <guptasahil9856@gmail.com>
2025-07-30 08:11:32 +0000

    learning dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,24 @@
+const id=Symbol('234');
+// const id2=Symbol('234');
+// // console.log(`id1:`+id+`id2:`+id2);
+// console.log(typeof id);
+// console.log(id2);
+// console.log(Symbol.for("Sahil"));
+
+// console.log(Symbol.keyFor(Symbol.for("Sahil")));
+// ********** conforming the reference data types *****
+
+
+// const arr=[3,2,5,4];
+// const arr2=arr;
+// arr2.splice(0,0,4)
+// console.log(arr);
+// console.log(arr2);
+// ************Conforming callbyValue dataTypes ********
+let num1=5;
+let num2=num1;
+num2=7;
+console.log(`num1: ${num1} and num2: ${num2}`);
+
+
+
