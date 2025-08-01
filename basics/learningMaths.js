2fc96e1538167d8611cd8e68ed7fea4bee8116b7
Sahil-dev71 <guptasahil9856@gmail.com>
2025-08-01 07:15:41 +0000

    learning about Maths and Number

---
+++ basics/learningMaths.js
@@ -0,0 +1,9 @@
+console.log(Math.min(5,6,76,34,7,2));
+const num=new Number(54.9877);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(5));
+const locale=10000000;
+console.log(locale.toLocaleString('en-In'));
+
+
