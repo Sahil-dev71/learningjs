904693b6511db317eedea699ae1df438a1ad6108
Sahil-dev71 <guptasahil9856@gmail.com>
2025-08-02 05:57:14 +0000

    learning Array

---
+++ basics/learningArray.js
@@ -0,0 +1,6 @@
+// const arr=new Array(3,4,5,6,7,8);
+const arr=[3,4,5,6,7,8]
+console.log(`A: ${arr}`);
+const spliceArr=arr.splice(1,1,2344,45,66);
+console.log(`A: ${spliceArr}`);
+console.log(`B: ${arr}`);
