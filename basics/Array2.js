c51cbbc873d00749ce773b0d86909295703085db
Sahil-dev71 <guptasahil9856@gmail.com>
2025-08-06 07:32:36 +0000

    learing array indepth

---
+++ basics/Array2.js
@@ -0,0 +1,5 @@
+const arr=[4,21,5,2,[345,45,[23]]]
+// console.log(arr);
+// console.log(arr.flat(Infinity))
+// console.log(Array.from("Sahil"));
+console.log(Array.from(Object.values({name:"Deepu",roll_no:34})))
