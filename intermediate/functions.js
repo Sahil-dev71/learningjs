2d0e8dd14876ac561691dd47f1846e69da1e7ec1
sahilgupta-88 <sahilgupta9856@gmail.com>
2025-08-08 12:01:10 +0530

    learing the concepts of functions

---
+++ intermediate/functions.js
@@ -0,0 +1,10 @@
+function sum(num1,num2){
+    if(!num1||!num2){
+        console.log("arguement to pass kr bro");
+        return;
+    }
+    return num1+num2;
+}
+const num1="sadf";
+const num2=4;
+console.log(`Sum Of The Fiven Arguments is : ${sum(num1,num2)}`);
