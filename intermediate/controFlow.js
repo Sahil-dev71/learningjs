960237f53b6f710345f368992a7439a1a737307e
sahilgupta-88 <sahilgupta9856@gmail.com>
2025-08-12 15:44:29 +0530

    learning concepts of nullish coalescing

---
+++ intermediate/controFlow.js
@@ -0,0 +1,16 @@
+const a=null??undefined??99;
+console.log(a);
+
+if(""){
+    console.log("this is empty string testing");
+}else{
+    console.log("falsy value of empty string");
+    
+}
+if(" "){
+    console.log("this is space string testing");
+}else{
+    console.log("falsy value of space string");
+    
+}
+
--- intermediate/executionOfJsCode.js
+++ intermediate/executionOfJsCode.js
@@ -1,10 +1,22 @@
-
-
 // var global.a=90;
-console.log(this==module.exports)
-global.c=34;
-console.log(globalThis.c);
-console.log();
+// console.log(this==module.exports)
+console.log(this==globalThis)
+
+// global.c=34;
+c=90;
+module.exports.fun=function name() {
+    console.log("heloo Dostoooo....");
+    
+}
+module.exports.name="SAhil";
+console.log(this);
+// console.log(global);
+// console.log(globalThis.c);
+// return 44;
+
+
+
+// console.log(global);
 /*
 Nodejs me js file ek function me run hoti h na ki globally or uss function ka naam module hota h tbhi var browser m window(jo ek
 global object h) sme add ho jata but Nodejs m nhi kyuki Nodejs m wo function m module scope m add ho jata h
